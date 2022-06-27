88309099d308f799010345e84e432fbc12a2b72a
Selim Sahin <selimsahin.sns@gmail.com>
2022-06-27 11:26:58 +0300

    database crud operations implemented

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const app = express();
-const tasks = require("./routes/tasks");
-const connectDB = require("./db/connect");
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
 require('dotenv').config()
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
 
 // Routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
 
-app.use("/api/v1/tasks", tasks);
+app.use('/api/v1/tasks', tasks)
 
-const port = 3000;
+const port = 3000
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`Server is listenin on port ${port}...`));
-  } catch (error) {
-    console.log(error);
-  }
-};
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listenin on port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-start();
+start()
